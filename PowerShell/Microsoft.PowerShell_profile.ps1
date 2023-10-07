b123157cdd0b9327007fe18a5f3f013e81e662fa
Pedro Mangino <pedromangino@outlook.com>
2023-10-07 10:48:39 -0300

    🔧 [chore] - Initial commit with config files

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,38 @@
+function touch {
+    Param(
+      [Parameter(Mandatory=$true)]
+      [string]$Path
+    )
+  
+    if (Test-Path -LiteralPath $Path) {
+      (Get-Item -Path $Path).LastWriteTime = Get-Date
+    } else {
+      New-Item -Type File -Path $Path
+    }
+  }
+
+# Importing some lazy functions from the https://github.com/BornToBeRoot/PowerShell project
+Import-Module LazyAdmin
+
+# importanto modulo para ter icones nos diretorios
+Import-Module -Name Terminal-Icons
+
+# # Ativando autocomplete igual linux
+# # Shows navigable menu of all options when hitting Tab
+# Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# # Autocompletion for arrow keys
+# Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+# Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+New-Alias -Name "tf" -Value "terraform"
+
+Invoke-Expression (&starship init powershell)
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
